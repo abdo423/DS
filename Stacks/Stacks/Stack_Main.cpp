5a60236f5cd03a88bacaa5cad7939e5a77ef471a
abdo423 <man-body@hotmail.com>
2022-04-13 22:33:08 +0200

    adding a push

--- Stacks/Stacks/Stack_Main.cpp
+++ Stacks/Stacks/Stack_Main.cpp
@@ -1,3 +1,32 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+
+@abdo423
+abdo423
+/
+DS
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+DS / Stacks / Stacks / Stack_Main.cpp
+@abdo423
+abdo423 added two push in stack slu
+Latest commit 3f1fb66 5 hours ago
+History
+1 contributor
+28 lines(23 sloc)  323 Bytes
+
 #include <iostream>
 #include "StackArr.h"
 
@@ -11,14 +40,19 @@ int main()
 	S.Push('c');
 	S.Push('e');
 	S.Push('d');
+	S.Push('f');
+	S.Push('g');
+	S.Push('h');
+	S.Push('i');
+	S.Push('j');
 
-	cout<<"top= "<<S.Top()<<endl;
+	cout << "top= " << S.Top() << endl;
 
-	while(!S.empty())
+	while (!S.empty())
 	{
-		cout<<S.Top()<<endl;
+		cout << S.Top() << endl;
 		S.Pop();
 	}
-	
+
 	return 0;
 }
