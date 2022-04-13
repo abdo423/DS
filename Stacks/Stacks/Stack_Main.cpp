4eddceb8c02f11745c5f01fa3e2dbc3d8f46b069
abdo423 <man-body@hotmail.com>
2022-04-13 22:49:58 +0200

    fixing

--- Stacks/Stacks/Stack_Main.cpp
+++ Stacks/Stacks/Stack_Main.cpp
@@ -1,31 +1,4 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
 
-@abdo423
-abdo423
-/
-DS
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-DS / Stacks / Stacks / Stack_Main.cpp
-@abdo423
-abdo423 added two push in stack slu
-Latest commit 3f1fb66 5 hours ago
-History
-1 contributor
-28 lines(23 sloc)  323 Bytes
 
 #include <iostream>
 #include "StackArr.h"
